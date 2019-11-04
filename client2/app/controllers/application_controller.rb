c6b8bf2aaff0e53640b949f5962f2f8d991da2cd
Tushar Tuteja <ttuteja@gmail.com>
2019-11-04 23:19:14 +0530

    basic sso complete for first blog entry on sso

--- client2/app/controllers/application_controller.rb
+++ client2/app/controllers/application_controller.rb
@@ -1,4 +1,33 @@
 # frozen_string_literal: true
 
+require 'httparty'
 class ApplicationController < ActionController::Base
+  def authenticated?
+    puts session[:user]
+    if session.nil? || session[:user].nil?
+      if params[:token].nil?
+        return redirect_to "http://localhost:3000?redirect_url=#{request.original_url}"
+      else
+        verify_url = "http://sso:3000/sso/verify_token?token=#{params[:token]}"
+        puts 'redirect_url', verify_url
+        response = HTTParty.get(verify_url)
+        if response['success'] = true
+          session['user'] = response['email']
+        else
+          return redirect_to "http://localhost:3000?redirect_url=#{request.original_url}"
+
+        end
+      end
+    end
+  end
+
+  def user_signed_in?
+    !session['user'].nil?
+  end
+
+  def current_user
+    session['user']
+  end
+
+  helper_method :user_signed_in?, :current_user
 end
--- client2/config/routes.rb
+++ client2/config/routes.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/logout'
+  get 'guest' => 'home#guest'
+  get '/' => 'home#index'
+  get '/sign_in' => 'application#sign_in'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
